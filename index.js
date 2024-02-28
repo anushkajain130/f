7f6719052fb0cf52add80220968fde77a48cb2ea
anushkajain130 <janushka986@gmail.com>
2024-02-28 21:52:35 +0530

    .

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const upload = multer({storage});
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser : true,
-    useUnifiedTopology : true,
+    useUnifiedTopology : true
 }).then(()=>{
     app.listen(PORT, ()=>{
         console.log(`Server is running on port ${PORT}`);
